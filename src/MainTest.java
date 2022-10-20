6c39d1f9ddd9711d277ca3174420bc4ad894279b
mikkelx <michalss1115@gmail.com>
2022-10-20 21:38:45 +0200

    XD

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 
 public class MainTest {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Seance s = new Seance(11);
-        FileService fs = new FileService();
-        fs.writeFile(s, "dupaa.txt");
-        Seance g = (Seance) fs.readFile("dupaa.txt");
-        System.out.println(g);
+        //Seance s = new Seance(11);
+        //FileService fs = new FileService();
+        //fs.writeFile(s, "dupaa.txt");
+        //Seance g = (Seance) fs.readFile("dupaa.txt");
+        //System.out.println(g);
     }
 }
